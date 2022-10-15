f31da871556d4a37cd3fe6662ff11950d0577e37
HypotheticalWhale <samueltan.9803@gmail.com>
2022-10-15 15:56:59 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "React Portfolio",
+    subtitle: "React, Tailwind, Netlify",
+    description:
+      "This exact Project! First project used to showcase my other work!",
+    image: "./portfolio.png",
+    link: "",
+  },
+  {
+    title: "Slames",
+    subtitle: "Godot",
+    description:
+      "Personal project created with a friend, we did the art and the music as well!",
+    image: "./slames.gif",
+    link: "https://phatwhale.itch.io/slames",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "He's a really smart and hardworking guy! Give him a chance and I'm sure you won't be disappointed!",
+    image: "./mum.jpg",
+    name: "My Mum",
+    company: "My House inc",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Python",
+  "Pandas",
+  "PowerBI",
+  "Godot"
+];
