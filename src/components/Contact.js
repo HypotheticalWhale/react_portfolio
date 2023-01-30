e828595b2af3ef5f9b938d5e74b189dbabf12dbb
HypotheticalWhale <samueltan.9803@gmail.com>
2023-01-30 18:27:59 +0800

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 export default function Contact() {
   const [name, setName] = React.useState("");
@@ -63,60 +64,14 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <form
-          netlify
-          name="test"
-          onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Work With Me
+            Work With Me:
           </h2>
-          <p className="leading-relaxed mb-5">
-            Contact me directly or drop me an notification via this input box!
-          </p>
-          <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
-            />
+          <div className="inline-flex py-1 px-3 my-6">
+                    <SocialIcon url="https://github.com/HypotheticalWhale" className="mr-4" target = "_blank/" fgColor="#fff" style={{height:35,width:35}} />
+                    <SocialIcon url="mailto:samueltan.9803@gmail.com" className="mr-4" target = "_blank/" fgColor="#fff" style={{height:35,width:35}}/>
+                    <SocialIcon url="https://www.linkedin.com/in/tanzhiweisamuel/" className="mr-4" target = "_blank/" fgColor="#fff" style={{height:35,width:35}}/>
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="relative mb-4">
-            <label
-              htmlFor="message"
-              className="leading-7 text-sm text-gray-400">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
-            />
-          </div>
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Submit
-          </button>
-        </form>
       </div>
     </section>
   );
--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "This exact Project! First project used to showcase my other work!",
     image: "./portfolio.png",
-    link: "",
+    link: "https://github.com/HypotheticalWhale/react_portfolio",
   },
   {
     title: "Slames",
@@ -14,6 +14,14 @@ export const projects = [
       "Personal project created with a friend, we did the art and the music as well!",
     image: "./slames.gif",
     link: "https://phatwhale.itch.io/slames",
+  },
+  {
+    title: "NFT Marketplace Application + Showcase",
+    subtitle: "React, React Native",
+    description:
+      "POC for an NFT marketplace application with a React frontend to showcase it",
+    image: "./logo.png",
+    link: "https://nft-showcase-react.vercel.app",
   },
 ];
 
